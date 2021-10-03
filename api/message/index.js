252af4acc16a035a656c5fe187a6c39788839419
albinalm <albin@codeindite.com>
2021-10-03 17:57:13 +0200

    Updated gitignore

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
     context.res.json({
-        text: "Hello from the API"
+        text: "Hello from the API. This message s recieved via a Azure Functions API."
     });
 };
